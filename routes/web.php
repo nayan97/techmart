6580e2fea4becf87129852157fc39ce110842fef
nayan97 <nayanislam085@gmail.com>
2024-08-04 13:15:16 +0600

    login validate

--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,16 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/admin/login', [AdminLoginController::class, 'index'])->name('admin.login');
+
+Route::group(['prefix' => 'admin',], function(){
+    Route::group(['middleware' => 'admin.guest'], function(){
+        Route::get('/login', [AdminLoginController::class, 'index'])->name('admin.login');
+        Route::post('/authenticate', [AdminLoginController::class, 'authenticate'])->name('admin.authenticate');
+        
+    });
+
+    Route::group(['middleware' => 'admin.auth'], function(){
+
+    });
+
+});
