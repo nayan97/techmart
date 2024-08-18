f97d094e8cff196e797acf689e69a48e77ad223d
nayan97 <nayanislam085@gmail.com>
2024-08-18 12:52:28 +0600

    temp image save

--- app/Http/Controllers/admin/TempImgController.php
+++ app/Http/Controllers/admin/TempImgController.php
@@ -2,10 +2,33 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Http\Controllers\Controller;
+use App\Models\TempImage;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class TempImgController extends Controller
 {
-    //
+    public function create(Request $request){
+
+        $image = $request->image;
+
+            if (!empty($image)) {
+
+                $ext = $image->getClientOriginalExtension();
+                $newName = time().'.'.$ext;
+
+                $tempImage = new TempImage();
+                $tempImage -> name = $newName;
+                $tempImage -> save();
+
+                $image->move(public_path().'/img/temp',$newName);
+
+                return response()->json([
+                    'status' => true,
+                    'image_id' => $tempImage->id,
+                    'message' => 'Image saved successfully',
+                ]);
+
+        }
+    }
 }
